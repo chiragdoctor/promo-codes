30f7b71bd60447a583346cc75111a0d17766b2e5
chiragdoctor <chirag.doctor@gmail.com>
2021-06-05 15:59:27 +0530

    Saving and getting user from localStorage

--- client/src/redux/user/user.reducer.js
+++ client/src/redux/user/user.reducer.js
@@ -1,15 +1,13 @@
 import { SET_USER } from '../../constants';
 
 const INITIAL_STATE = {
-  user: {
-    token: localStorage.getItem('token'),
-  },
+  user: JSON.parse(localStorage.getItem('user')),
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case SET_USER: {
-      localStorage.setItem('token', action.user.token);
+      localStorage.setItem('user', JSON.stringify(action.user));
       return {
         ...state,
         user: { ...action.user, isAuthenticated: true },
