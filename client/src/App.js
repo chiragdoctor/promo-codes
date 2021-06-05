de7c2685b1deb4546999c635eff337c430d033ad
chiragdoctor <chirag.doctor@gmail.com>
2021-06-05 16:36:18 +0530

    Add Dashboard private route and bacground color

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
+import Header from './components/Header';
 import Login from './components/Login';
 import PrivateRoute from './components/PrivateRoute';
+import Sidebar from './components/Sidebar';
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/login' component={Login} />
-        <PrivateRoute exact path='/' component={Dashboard} />
-      </Switch>
-    </Router>
+    <div className='container'>
+      <Router>
+        <Sidebar />
+        <Header />
+        <Switch>
+          <Route exact path='/login' component={Login} />
+          <PrivateRoute exact path='/' component={Dashboard} />
+        </Switch>
+      </Router>
+    </div>
   );
 };
 
